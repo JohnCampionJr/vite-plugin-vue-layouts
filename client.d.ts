4b069b2ba370c3c5a70878dc7d728cba837e12a5
Martin Mädler <martin.maedler@gmail.com>
2021-07-26 16:34:58 +0200

    Fix typings
    Add missing return type annotations.
    Add missing Component import.
    
    Fixes #30

--- client.d.ts
+++ client.d.ts
@@ -1,11 +1,13 @@
 declare module 'layouts-generated' {
   import { RouteRecordRaw } from 'vue-router'
-  export function setupLayouts(routes: RouteRecordRaw[])
+  export function setupLayouts(routes: RouteRecordRaw[]): RouteRecordRaw[]
 }
 
 declare module 'virtual:generated-layouts' {
+  import { Component } from 'vue'
   import { RouteRecordRaw } from 'vue-router'
-  export function setupLayouts(routes: RouteRecordRaw[])
+  export function setupLayouts(routes: RouteRecordRaw[]): RouteRecordRaw[]
   export function createRouterLayout(
-    resolve: (layoutName: string) => Promise<Component | { default: Component }>)
+    resolve: (layoutName: string) => Promise<Component | { default: Component }>
+  ): Component
 }

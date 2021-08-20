a9ed68b4ea843b741c1fe408fbd4178baddf03c8
Giovane Souza <giovane.bds@gmail.com>
2021-08-20 19:15:32 -0300

    Add missing types
    Added missing types for `setupLayouts` and `createRouterLayout` function returns.

--- client.d.ts
+++ client.d.ts
@@ -1,11 +1,12 @@
 declare module 'layouts-generated' {
   import { RouteRecordRaw } from 'vue-router'
-  export function setupLayouts(routes: RouteRecordRaw[])
+  export function setupLayouts(routes: RouteRecordRaw[]): RouteRecordRaw[]
 }
 
 declare module 'virtual:generated-layouts' {
   import { RouteRecordRaw } from 'vue-router'
-  export function setupLayouts(routes: RouteRecordRaw[])
+  import { Component } from 'vue'
+  export function setupLayouts(routes: RouteRecordRaw[]): RouteRecordRaw[]
   export function createRouterLayout(
-    resolve: (layoutName: string) => Promise<Component | { default: Component }>)
+    resolve: (layoutName: string) => Promise<Component | { default: Component }>): Promise<Component | { default: Component }>
 }

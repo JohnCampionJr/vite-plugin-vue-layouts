4fd2d78404fc3f136a81d0746e422495f370af2c
John Campion Jr <john@brightshore.com>
2023-12-04 17:20:52 -0500

    fix: old vite2 regex, closes #124

--- src/clientSide.ts
+++ src/clientSide.ts
@@ -9,7 +9,7 @@ function normalizePath(path: string) {
 export async function isVite2() {
   const info = await getPackageInfo('vite')
   if (info)
-    return /.?2/.test(info.version)
+    return parseInt(info.version) === 2
 
   return false
 }

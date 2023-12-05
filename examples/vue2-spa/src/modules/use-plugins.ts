6d924b3a4aba3dc1c5fe5c634dfa967dbb58dc75
John Campion Jr <john@brightshore.com>
2023-12-05 12:54:54 -0500

    chore: remove vue2; it is eol as of 12/31/23

--- examples/vue2-spa/src/modules/use-plugins.ts
+++
@@ -1,8 +0,0 @@
-import { VueConstructor } from 'vue'
-import * as ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import 'element-ui/lib/theme-chalk/display.css'
-
-export default (app: VueConstructor) => {
-  app.use(ElementUI)
-}
--- examples/vue2-spa/src/shims-vue.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.vue' {
-  import Vue from 'vue'
-  export default Vue
-}
-
-declare module '@vue/runtime-dom' {
-  export * from '@vue/runtime-dom/dist/runtime-dom'
-  export { defineComponent, PropType } from '@vue/composition-api'
-}
